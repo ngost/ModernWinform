d971ee114365b85d45016849017fcb7410ef114c
ngost <hm5207@gmail.com>
2021-01-11 19:16:48 +0900

    hellow world button and label added.

--- ModernWinform/Form1.Designer.cs
+++ ModernWinform/Form1.Designer.cs
@@ -29,13 +29,55 @@ namespace ModernWinform
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.Font = new System.Drawing.Font("굴림", 15F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(478, 276);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 107);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Action";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 25F);
+            this.label1.Location = new System.Drawing.Point(525, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Empty";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1211, 663);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("굴림", 15F);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ModernWinform/Form1.cs
+++ ModernWinform/Form1.cs
@@ -16,5 +16,15 @@ namespace ModernWinform
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "hellow World!";
+        }
     }
 }
